7169f11002df53611ed6ac6559df746cdbae1d02
A601-13 <tnk.dat.class@gmail.com>
2022-06-09 13:39:02 +0900

    ボーナス課題

--- fukuv0609/Form1.Designer.cs
+++ fukuv0609/Form1.Designer.cs
@@ -34,6 +34,10 @@ namespace fukuv0609
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -67,11 +71,55 @@ namespace fukuv0609
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(462, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 68);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "↑";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "←";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(539, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 68);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "→";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(462, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 68);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "↓";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -88,6 +136,10 @@ namespace fukuv0609
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- fukuv0609/Form1.cs
+++ fukuv0609/Form1.cs
@@ -20,12 +20,37 @@ namespace fukuv0609
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("玉井秀兵");
+            timer1.Enabled = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += int.Parse(textBox1.Text);
             label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top += -1;
+            label1.Text = "(/・ω・)/";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left += -1;
+            label1.Text = "_(・ω・_)";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left += 1;
+            label1.Text = "(_・ω・)_";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top += 1;
+            label1.Text = "(・ω・)";
         }
     }
 }
